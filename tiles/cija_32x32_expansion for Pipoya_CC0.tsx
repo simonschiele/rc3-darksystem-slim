d8b489c930abd9b611d05a6e214e629b763bb2a4
Simon Schiele <simon.codingmonkey@gmail.com>
2020-12-27 19:13:39 +0100

    Import from old repo
    Imprting from old dirty repo. Original repo located at:
    https://gitlab.com/darksystem/rc3-darksystem

---
+++ tiles/cija_32x32_expansion for Pipoya_CC0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="cija_32x32_expansion for Pipoya_CC0" tilewidth="32" tileheight="32" tilecount="1995" columns="15" backgroundcolor="#898989">
+ <image source="tiles.rc3.world/community/bytewerk/cija_32x32_expansion for Pipoya_CC0.png" width="480" height="4256"/>
+</tileset>
---
+++ tiles/starBackground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="starBackground" tilewidth="32" tileheight="32" tilecount="56" columns="7">
+ <image source="darksystem/Archive/Space Shooter Assets/PNG/Background/starBackground.png" width="254" height="256"/>
+</tileset>
